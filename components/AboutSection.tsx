4b50061a0cb88d3f3d19a7a10c809c4ba2cb3c4e
Ziad Adel Ismail <ziad.adel.mahmoud@gmail.com>
2023-05-03 23:57:12 +0300

    initial commit

---
+++ components/AboutSection.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import Image from "next/image";
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+];
+
+const AboutSection = () => {
+  return (
+    <section className="font-mont" id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Ziad and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" driven"}</span> software developer
+              based in Cairo, EG.
+            </p>
+            <br />
+            <p>
+              I graduated from London South Bank University, in 2021 with a BS
+              in International Business and after one year of experience as a
+              growth hacker at a YC-startup I decided to shift my career to
+              software development
+            </p>
+            <br />
+            <p>
+              I have a wide range of hobbies and passions that keep me busy.
+              From reading, playing sports, watching movies, to writing blogs on
+              Medium, I am always seeking new challenging experiences and love
+              to keep myself engaged and learning new things to make sure that I
+              continue my self-improvement journey.
+            </p>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-teal-500">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I strive to do, I have a passion for
+              technology and entrepreneurship and a desire to always push the
+              limits of what is possible. I am excited to see where my career
+              takes me and am always open to new opportunities. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                );
+              })}
+            </div>
+            <Image
+              src="/ziad-adel2.jpg"
+              alt=""
+              width={225}
+              height={225}
+              className="hidden md:block md:relative md:bottom-[-3rem] md:left-32 md:z-0 rounded-[50%]"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ components/Footer.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  AiOutlineGithub,
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+  AiOutlineMedium,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+      <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
+      <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
+          © 2023 Ziad Adel<a href="/" className="hover:underline"></a>
+        </div>
+        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
+          <a
+            href="https://github.com/ZiadAdel10"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineGithub
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+          <a
+            href="https://twitter.com/Ziad_Thoughts"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineTwitter
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/ziad-adel-swe/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineLinkedin
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+          <a
+            href="https://medium.com/@ziadadelmahmoud"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineMedium
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
