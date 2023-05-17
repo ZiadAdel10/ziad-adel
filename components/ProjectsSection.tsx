99cc54300ca7da4e651f0322b5f431ad21d9f6eb
Ziad Adel Ismail <ziad.adel.mahmoud@gmail.com>
2023-05-17 17:13:56 +0300

    add new projects

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -6,13 +6,12 @@ import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
 
 const projects = [
   {
-    name: "Quizzies",
+    name: "Password Generator",
     description:
-      "Quizzies is a fun trivia game where you try your best to answer 5 questions right in your chosen quiz category.",
-    image: "/quizzies.png",
-    tech: "React, API, CSS",
-    github: "https://github.com/ZiadAdel10/quizzies_game_project",
-    link: "https://main--quizzies-by-ziad.netlify.app/",
+      "A greate password generator to copy strong customized passwords",
+    image: "/password-generator.png",
+    github: "https://github.com/ZiadAdel10/password-generator",
+    link: "https://password-generator-eight-coral.vercel.app/",
   },
   {
     name: "Prompt Finder",
@@ -23,12 +22,54 @@ const projects = [
     link: "https://promptfinder.vercel.app/",
   },
   {
+    name: "Quizzies",
+    description:
+      "Quizzies is a fun trivia game where you try your best to answer 5 questions right in your chosen quiz category.",
+    image: "/quizzies.png",
+    tech: "React, API, CSS",
+    github: "https://github.com/ZiadAdel10/quizzies_game_project",
+    link: "https://main--quizzies-by-ziad.netlify.app/",
+  },
+  {
+    name: "Snowbot Weather Forecaster",
+    description:
+      "its a weather forecast app where you enter the location you want to search for and get their weather",
+    image: "/weather-bot.png",
+    tech: "JavaScript",
+    github: "https://github.com/ZiadAdel10/weather-app-project",
+    link: "https://ziad-adel-weather-app.netlify.app/",
+  },
+  {
+    name: "Simple Calculator",
+    description: "simple calculator app",
+    image: "/simple-calc.png",
+    tech: "JavaScript",
+    github: "https://github.com/ZiadAdel10/simple-calculator",
+    link: "https://simple-calculator-dun.vercel.app/",
+  },
+  {
     name: "Omnifood",
     description: "Omnifood is an online food catering web app",
     image: "/omnifood.png",
     tech: "Vanilla JS, CSS",
     github: "https://github.com/ZiadAdel10/omnifood-CSS-project",
     link: "https://omnifood-ziadadel.netlify.app/",
+  },
+  {
+    name: "EdgeLedger",
+    description: "Financial service app landing page",
+    image: "/edge-ledger.png",
+    tech: "CSS",
+    github: "https://github.com/ZiadAdel10/edge-ledger",
+    link: "https://edge-ledger-alpha.vercel.app/",
+  },
+  {
+    name: "HBT Hotel",
+    description: "Hotel Landing Page",
+    image: "/hotel-landing.png",
+    tech: "CSS",
+    github: "https://github.com/ZiadAdel10/hotel-website",
+    link: "https://hotel-website-hazel.vercel.app/",
   },
 ];
 
